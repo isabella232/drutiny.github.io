9ddc5b0cd9e63a719df1c1d1f28443b6ec041f3e
Travis CI <drutiny@travis.ci>
2018-03-20 12:11:36 +0000

    Deploy from Travis CI

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -ex
+git checkout $TRAVIS_BRANCH
 
-ssh-add docs/drutiny_travis-id_rsa
+openssl aes-256-cbc -K $encrypted_9472126ed793_key -iv $encrypted_9472126ed793_iv -in docs/ghp-id_rsa.enc -out ghp-id_rsa -d
+chmod 0400 ghp-id_rsa
+IDENTITY_FILE="`pwd`/ghp-id_rsa"
+export GIT_SSH_COMMAND="ssh -i $IDENTITY_FILE"
 
-# sudo pip install -r docs/requirements.txt
-# composer config minimum-stability dev
-# composer require --prefer-stable  drutiny/plugin-distro-common drutiny/acquia drutiny/sumologic drutiny/http drutiny/plugin-drupal-7 drutiny/plugin-drupal-8 drutiny/cloudflare
+sudo pip install -r docs/requirements.txt
+composer config minimum-stability dev
+composer require --prefer-stable  drutiny/plugin-distro-common drutiny/acquia drutiny/sumologic drutiny/http drutiny/plugin-drupal-7 drutiny/plugin-drupal-8 drutiny/cloudflare
 
-./bin/build_docs
+./bin/build_docs 2>/dev/null
 mkdocs build --clean
 
 if [ -d ghp ]; then
